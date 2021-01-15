7123bf016f9d5e4267aa41039020a2430a657f25
Michael Dinh <tonkoalsce@gmail.com>
2021-01-15 13:06:33 +1100

    added a typing animation using CL label

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
-
+import CLTypingLabel
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = "🐸PepeChat"
+        
+//        var charIndex = 0.0
+//        titleLabel.text = ""
+//        let titleText = "🐸PepeChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//                print(letter)
+//            }
+//            charIndex += 1
+//        }
+        
     }
     
 
